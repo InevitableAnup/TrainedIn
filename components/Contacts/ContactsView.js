9de0041b48e3ed35de5d22d89ca3f95f8ae3c5af
Anup <anup@inevitablesol.com>
2017-11-22 12:47:07 +0530

    22-11-2017 Completed implementing List view with swipe for info and swipe for delete with data from firebase database.

--- components/Contacts/ContactsView.js
+++ components/Contacts/ContactsView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, ListView } from "react-native";
 import {
   Container,
   Header,
@@ -17,32 +17,85 @@ import {
   Form,
   Toast,
   Button,
-  Root
+  Text,
+  Root,
+  List,
+  ListItem
 } from "native-base";
 
+import { SearchBar } from "react-native-elements";
+
+const datas = [
+  'Simon Mignolet',
+  'Nathaniel Clyne',
+  'Dejan Lovren',
+  'Mama Sakho',
+  'Alberto Moreno',
+  'Emre Can',
+  'Joe Allen',
+  'Phil Coutinho',
+];
+
 export default class ContactsView extends React.Component {
-  static navigationOptions = {
-    title: "Contacts View"
-  };
+
+  constructor(props) {
+    super(props);
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      basic: true,
+      listViewData: datas,
+    };
+  }
+
+  deleteRow(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.listViewData];
+    newData.splice(rowId, 1);
+    this.setState({ listViewData: newData });
+  }
+
+
   render() {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     return (
       <Container>
-        <Header style={{ marginTop: "5%" }}>
-          <Left>
-            <Button
-              transparent
-              onPress={() => this.props.navigation.navigate("DrawerOpen")}
-            >
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Contacts</Title>
-          </Body>
-          <Right />
+        <Header searchBar rounded style={{ marginTop: "5%" }}>
+          <Button
+            transparent
+            onPress={() => this.props.navigation.navigate("DrawerOpen")}
+          >
+            <Icon name="menu" />
+          </Button>
+
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+            <Icon name="ios-people" />
+          </Item>
+          <Button transparent>
+          <Icon name="search" />
+        </Button>
         </Header>
         <Content>
-          <Text>ContactsView</Text>
+        
+        <List
+        dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+        renderRow={data =>
+          <ListItem>
+            <Text> {data} </Text>
+          </ListItem>}
+        renderLeftHiddenRow={data =>
+          <Button full onPress={() => alert(data)}>
+            <Icon active name="information-circle" />
+          </Button>}
+        renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+          <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
+            <Icon active name="trash" />
+          </Button>}
+        leftOpenValue={75}
+        rightOpenValue={-75}
+      />
+
         </Content>
       </Container>
     );
--- components/Training/TrainingView.js
+++ components/Training/TrainingView.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import * as firebase from "firebase";
+import { StyleSheet, Text, View, ListView } from "react-native";
+
 import {
   Container,
   Header,
@@ -19,16 +19,76 @@ import {
   Toast,
   Button,
   Root,
-  Grid,
-  Col
+  List,
+  ListItem
 } from "native-base";
 
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+import * as firebase from "firebase";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC1ikrKiKxmKJUFpAp3aiFOtJ46rSbA0Pw",
+  authDomain: "trainedin-c2c10.firebaseapp.com",
+  databaseURL: "https://trainedin-c2c10.firebaseio.com",
+  projectId: "trainedin-c2c10",
+  storageBucket: "trainedin-c2c10.appspot.com"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 
 export default class TrainingView extends React.Component {
+
+  constructor(props){
+    super(props);
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1,r2) => r1 !== r2 });
+    this.state ={
+      basic: true,
+      itemDataSource: ds,
+      
+    }
+    this.itemsRef = this.getRef().child('trainings').child('wqecTJUTZXbOZRJd9hW1eETE9gK2');
+  }
+
+  getRef(){
+    return firebaseApp.database().ref();
+  }
+
+  componentDidMount() {
+    this.getItems(this.itemsRef);
+  }
+
+  componentWillMount() {
+    this.getItems(this.itemsRef);
+  }
+
+  getItems(itemsRef) {
+    itemsRef.on('value',(snap) => {
+      let items=[];
+      snap.forEach((child) => {
+        
+        console.log(child.val().trainingName);
+        items.push(child.val().trainingName);
+      });
+      this.setState({
+        itemDataSource: items
+      });
+    });
+
+
+  }
+
+  deleteRow(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.itemDataSource];
+    newData.splice(rowId, 1);
+    this.setState({ itemDataSource: newData });
+  }
+  
   render() {
+    
     return (
       <Container>
         <Header style={{ marginTop: "5%" }}>
@@ -46,9 +106,26 @@ export default class TrainingView extends React.Component {
           <Right />
         </Header>
         <Content>
-          <KeyboardAwareScrollView>
-            <Text>Training View</Text>
-          </KeyboardAwareScrollView>
+          
+        <List
+        dataSource={this.ds.cloneWithRows(this.state.itemDataSource)}
+        renderRow={data =>
+          <ListItem>
+            <Text> {data} </Text>
+          </ListItem>}
+        renderLeftHiddenRow={data =>
+          <Button full onPress={() => alert(data)}>
+            <Icon active name="information-circle" />
+          </Button>}
+        renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+          <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
+            <Icon active name="trash" />
+          </Button>}
+        leftOpenValue={75}
+        rightOpenValue={-75}
+      />
+
+          
         </Content>
       </Container>
     );
